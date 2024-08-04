23b834b9ca083dcbf90909ab7e0f008f898deb98
Alejo <alejooodaniel45@gmail.com>
2024-08-04 18:55:39 -0300

    Subida de menu y testeo hosting

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/"
-})
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === 'build' ? '/Final-Frontend-UTN-TM/' : '/', // Ajusta el base para producción
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+      },
+    },
+  };
+});
