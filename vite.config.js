7fd9f2d79a5ee6666d9d5e7c9e4de0a4243b9f09
Alejo <alejooodaniel45@gmail.com>
2024-08-04 19:26:28 -0300

    testeo de hosting

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ command }) => {
   return {
     plugins: [react()],
-    base: command === "build" ? "/Final-Frontend-UTN-TM/" : "/",
+    base:
+      command === "build"
+        ? "https://illualex.github.io/Final-Frontend-UTN-TM"
+        : "/",
   };
 });
